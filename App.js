cf73ee061e7ba9a877eebb3284f8ea76d942f1e5
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-02-19 07:39:49 +0200

    Asynchronous-Basics

---
+++ App.js
@@ -0,0 +1,21 @@
+console.log("starting....")
+
+
+
+
+setTimeout(()=>{
+
+console.log("2 Seconds Timer")
+
+},2000)
+
+
+setTimeout(()=>{
+
+    console.log("0 Seconds Timer")
+    
+    },00)
+    
+
+
+console.log("stopping....")
