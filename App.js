fd80351a1ff45214b3fa94295ea8cf1493153a5d
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-02-20 08:09:44 +0200

    Cleaner Code

--- App.js
+++ App.js
@@ -6,14 +6,14 @@ const fs = require('fs');
 const url = 'http://api.weatherstack.com/current?access_key=1ce8a0b5e1f91866a138bab7eb732a61&query=37.8267,-122.4233'
 
 
-request({url:url},(error,response)=>{
+request({url:url , json:true},(error,response)=>{
 
-    const RawData= response
-    const JsonData = JSON.parse(RawData.body)
-   // console.log(JsonData)
-    const BufferData= JSON.stringify(JsonData)
-    fs.writeFileSync('WeatherData.Json',BufferData)
+//     const RawData= response
+//     const JsonData = JSON.parse(RawData.body)
+//    // console.log(JsonData)
+//     const BufferData= JSON.stringify(JsonData)
+//     fs.writeFileSync('WeatherData.Json',BufferData)
     
-    console.log("the temp. is  "+ JsonData.current.temperature+"  but it feels like  "+JsonData.current.feelslike)
+    console.log("the temp. is  "+ response.body.current.temperature+"  but it feels like  "+response.body.current.feelslike)
 
 })
