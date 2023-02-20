5ca00a02eea89c4f9612828d9a5336ae1edfe6da
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-02-20 08:14:06 +0200

    changed Temp Units

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 
-const url = 'http://api.weatherstack.com/current?access_key=1ce8a0b5e1f91866a138bab7eb732a61&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=1ce8a0b5e1f91866a138bab7eb732a61&query=37.8267,-122.4233&units=f'
 
 
 request({url:url , json:true},(error,response)=>{
@@ -14,6 +14,6 @@ request({url:url , json:true},(error,response)=>{
 //     const BufferData= JSON.stringify(JsonData)
 //     fs.writeFileSync('WeatherData.Json',BufferData)
     
-    console.log("the temp. is  "+ response.body.current.temperature+"  but it feels like  "+response.body.current.feelslike)
+    console.log("the temp. is  "+ response.body.current.temperature+"  degree out  " +" but it feels like  "+response.body.current.feelslike+"  degree out  ")
 
 })
