5c25504ae15f321b49f5c26e539ea848ccfc382e
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-02-21 08:50:07 +0200

    Error handling

--- App.js
+++ App.js
@@ -3,30 +3,67 @@ const fs = require('fs');
 
 
 
-const url = 'http://api.weatherstack.com/current?access_key=1ce8a0b5e1f91866a138bab7eb732a61&query=37.8267,-122.4233&units=f'
+const url = 'http://api.weatherstack.com/current?access_key=1ce8a0b5e1f91866a138bab7eb732a61&query=37.8267,-122&units=f'
 
 
 request({url:url , json:true},(error,response)=>{
 
-//     const RawData= response
-//     const JsonData = JSON.parse(RawData.body)
-//    // console.log(JsonData)
-//     const BufferData= JSON.stringify(JsonData)
-//     fs.writeFileSync('WeatherData.Json',BufferData)
+
     
+if (error) {
+    //low level error handling
+    console.log("unable to connect to the Weather Stack service")
+
+
+}else if(response.body.error){
+
+
+    console.log("Unable to find location")
+
+
+} else {
+
+    //     const RawData= response
+    //     const JsonData = JSON.parse(RawData.body)
+    //    // console.log(JsonData)
+    //     const BufferData= JSON.stringify(JsonData)
+    //     fs.writeFileSync('WeatherData.Json',BufferData)
     console.log(response.body.current.weather_descriptions[0]+ " .the temp. is  "+ response.body.current.temperature+"  degree out  " +" but it feels like  "+response.body.current.feelslike+"  degree out  ")
+
+}
 
 })
 
 
 
-const GeoUrl='http://api.positionstack.com/v1/forward?access_key=950cf51aa05520dd8fa6fa83a127c7dc&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC'
+const GeoUrl='http://api.positionstack.com/v1/forward?access_key=950cf51aa05520dd8fa6fa83a127c7dc&query=1600%255Pennsylvania%20Ave%20NW,%20Washingtons%20DC'
 
 
 request({url :GeoUrl,json:true},(error,response)=>{
 
-console.log(response.body.data[0].latitude)
-console.log(response.body.data[0].longitude)
 
+
+if(error){
+        //low level error handling
+    console.log('unable to connet to the GeoCoding Service')
+
+}else if(response.body.data[0].length===0){
+    console.log("unable to find location , try another search ")
+
+
+
+
+console.log("unable to find the location")
+
+
+
+}else{
+
+    const latitude = response.body.data[0].latitude
+    const longitude = response.body.data[0].longitude
+
+    console.log(latitude)
+    console.log(longitude)
+}
 
 })
